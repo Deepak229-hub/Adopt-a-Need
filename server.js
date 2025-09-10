2dceb1eb6afc641baf90ef81cb78ab87bd4b485c
Deepak Tiwari <tiwarideepak.dk.2004@gmail.com>
2025-09-10 19:30:43 +0530

    Created session

--- server.js
+++ server.js
@@ -4,12 +4,26 @@ import ejs from "ejs"
 import pg from "pg"
 import bcrypt from "bcrypt"
 import env from "dotenv"
-
+import session from "express-session"
+import passport from "passport"
+import { Strategy } from "passport-local"
 
 const app = express()
 const port = 3000
 const saltRounds = 10
 env.config()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const db = new pg.Client({
     user: process.env.PG_USER,
@@ -19,9 +33,6 @@ const db = new pg.Client({
     port: 5432
 })
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"))
-
 db.connect()
 
 app.get("/", (req, res) => {
@@ -29,64 +40,84 @@ app.get("/", (req, res) => {
 })
 
 app.get("/login", (req, res) => {
-    res.render("partials/login.ejs")
+    res.render("login.ejs")
 })
 
 app.get("/register", (req, res) => {
-    res.render("partials/register.ejs")
+    res.render("login.ejs")
 })
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}))
 
 app.post("/register", async (req, res) => {
     const name = req.body.name
-    const email = req.body.email
-    const phone = req.body.phone
-    let password = null
-    if (req.body.password == req.body.confirm_password) {
-        password = req.body.password
+    const email = req.body.username
+    const password = req.body.password
+    const confirmPassword = req.body.confirm_password
+    if (password.length < 8) {
+        return res.render("login.ejs", { errorMessage: "Password must be 8 characters long!" })
     }
-
+    if (password !== confirmPassword) {
+        return res.render("login.ejs", { errorMessage: "Confirm Password must be same to the entered password!" })
+    }
     try {
-        let user = await db.query("SELECT * FROM users WHERE email = $1", [email])
-        if (user.rows.length > 0) {
-            res.send("User already exists!");
+        let result = await db.query("SELECT * FROM users WHERE email=$1", [email])
+        if (result.rows.length > 0) {
+            return res.render("login.ejs", { errorMessage: "User already exists!" })
         } else {
             bcrypt.hash(password, saltRounds, async (err, hash) => {
                 if (err) {
-                    console.log("Error hashing password: ", err)
-                } else {
-                    const result = await db.query("INSERT INTO users (name, email, phone, password) VALUES ($1, $2, $3, $4)", [name, email, phone, hash])
-                    res.send("User Registered Successfully!")
+                    return res.status(500).send("Failed to register right now. Pleasr try again!")
                 }
+                const result = await db.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *", [name, email, hash])
+                const user = result.rows[0]
+                req.login(user, (err) => {
+                    if (err) {
+                        return res.status(500).send("Login failed after registration!")
+                    }
+                    res.redirect("/")
+                })
             })
         }
     } catch (error) {
-        console.log("An Error occured!")
+        res.status(500).send("An error occured during register!")
     }
 })
 
-app.post("/login", async (req, res) => {
-    const user = req.body.email || req.body.phone
-    const password = req.body.password
-
+passport.use(new Strategy(async function verify(username, password, cb) {
     try {
-        let result = await db.query("SELECT * FROM users WHERE email = $1 OR phone = $1", [user])
-        if(result.rows.length > 0) {
-            const userPassword = result.rows[0].password
+        let result = await db.query("SELECT * FROM users WHERE email=$1", [username])
+        if (result.rows.length > 0) {
+            const user = result.rows[0]
+            const userPassword = user.password
             bcrypt.compare(password, userPassword, (err, result) => {
-                if(err) {
-                    console.log(err)
+                if (err) {
+                    console.log("error comparing password")
+                    return cb(err)
                 } else if (result) {
-                    res.send("User Logged in!")
+                    return cb(null, user)
                 } else {
-                    res.send("Wrong password!")
+                    return cb(null, false)
                 }
             })
         } else {
-            res.send("User doesn't exists!")
+            return cb(null, false, {message: "Incorrect Username or Password!"})
         }
     } catch (error) {
-        console.log(error)
+        console.log(error.message)
+        return cb(error)
     }
+}))
+
+passport.serializeUser((user, cb) => {
+    cb(null, user)
+})
+
+passport.deserializeUser((user, cb) => {
+    cb(null, user)
 })
 
 app.listen(port, () => {
